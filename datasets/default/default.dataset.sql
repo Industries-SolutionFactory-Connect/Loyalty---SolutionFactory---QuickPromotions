666c83e9ccae9954ef58d27d6e5707017c940416
athorthi <96427730+athorthi@users.noreply.github.com>
2023-03-02 17:01:36 +0530

    Update default.dataset.sql
    added new records locally

--- datasets/default/default.dataset.sql
+++ datasets/default/default.dataset.sql
@@ -8,8 +8,8 @@ CREATE TABLE "LoyaltyProgram" (
 	"Status" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "LoyaltyProgram" VALUES(1,'Primary loyalty program for NTO','','True','NTO Insider','Active');
-INSERT INTO "LoyaltyProgram" VALUES(2,'','','False','Sample Program','Active');
+INSERT INTO "LoyaltyProgram" VALUES(1,'Primary loyalty program for NTO','','False','NTO Insider 2','Active');
+INSERT INTO "LoyaltyProgram" VALUES(2,'','','False','Sample Program 2','Active');
 CREATE TABLE "LoyaltyTierGroup" (
 	id INTEGER NOT NULL, 
 	"IsActive" VARCHAR(255), 
@@ -27,6 +27,6 @@ CREATE TABLE "LoyaltyTierGroup" (
 	"LoyaltyProgramId" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "LoyaltyTierGroup" VALUES(1,'True','','None','False','Months','12','False','2023-03-30','2','Years','Tier One','Fixed','2');
-INSERT INTO "LoyaltyTierGroup" VALUES(2,'True','Tier members based on annual spend','None','False','Years','1','True','2022-12-31','1','Years','Status Tier Group','Fixed','1');
+INSERT INTO "LoyaltyTierGroup" VALUES(1,'True','','None','False','Months','12','False','2023-03-30','2','Years','Tier One 1','Fixed','2');
+INSERT INTO "LoyaltyTierGroup" VALUES(2,'True','Tier members based on annual spend','None','False','Years','1','True','2022-12-31','1','Years','Status Tier Group 1','Fixed','1');
 COMMIT;
